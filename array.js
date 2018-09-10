e36df092a831556eb9559e81e5117abe9a897e63
hirokisan <expsharetoworld@gmail.com>
2018-09-10 17:45:02 +0900

    Add array.js

---
+++ array.js
@@ -0,0 +1,15 @@
+var position = [];
+position[0] = [0, 0];
+position[1] = [0, 320];
+
+console.log(position[0][0]);
+console.log(position[0][1]);
+console.log(position[1][0]);
+console.log(position[1][1]);
+
+var array = [];
+for(var i = 0; i < 4; i++){
+  array[i] = i;
+}
+
+console.log(array);
